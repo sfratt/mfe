451f7f378c4cc49dec68128b6423dca6b60b3b36
Stephen Frattaroli <sfratt@users.noreply.github.com>
2023-11-22 19:25:34 -0500

    Render the React components via App.js to the DOM

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// Mount function to start up the app
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+
+// If in development and in isolation, call `mount` immediately
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing-dev-root");
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+// App is running throught the container and `mount` should be exported
+export { mount };
